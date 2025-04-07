403158358127a2320424af643b55d3c983ac2bc8
Vijaianand Mohan <vijaianand@gmail.com>
2025-04-07 17:53:40 +0530

    Query Params Editor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import URLDecoder from "./pages/URLDecoder";
 import "./index.css";
 import RemoveLineBreaks from "./pages/RemoveLineBreaks";
 import RenderString from "./pages/RenderString";
+import RenderMarkdown from "./pages/RenderMarkdown";
+import QueryParamEditor from "./pages/QueryParamEditor";
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
         <Route path="/tools/url-decoder" element={<URLDecoder />} />
         <Route path="/tools/remove-line-breaks" element={<RemoveLineBreaks />} />
         <Route path="/tools/render-string" element={<RenderString />} />
+        <Route path="/tools/render-markdown" element={<RenderMarkdown />} />
+        <Route path="/tools/query-param-editor" element={<QueryParamEditor />} />
       </Routes>
     </Router>
   );

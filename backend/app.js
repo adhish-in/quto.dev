4156f8e38095d308b4206224073f6843a3b063a2
Vijaianand Mohan <vijaianand@gmail.com>
2025-04-17 13:32:03 +0530

    Increase limit to 5 MB

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(cors());
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ extended: true, limit: '5mb' }));
 app.use(express.json());
 
 // Shared health check handler
